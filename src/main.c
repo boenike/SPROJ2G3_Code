5a51493d7abbab4392f05a15496700877c47cd10
Bence Tóth <bencike04201@gmail.com>
2024-04-16 19:31:35 +0200

    Rewrote frontend for the OLED display
    Rewrote frontend for the OLED display

--- src/main.c
+++ src/main.c
@@ -3,21 +3,17 @@
  * File: main.c
  * Author: Bence Toth
  * Year: 2024
- * Date: 28/02/2024
+ * Date: 16/04/2024
  * Semester: 2nd
  * Course: BEng in Electronics
+ * Display: 0.96" SSD1306 OLED (128x64) via I2C
  */
-
-// Set the Clock Frequency to 16 MHz
-#ifndef F_CPU
-#define F_CPU 16000000UL
-#endif
 
 // Constants
 #define ROW_LEN 21
-#define NUM_LEN 10
-#define ADC_PIN PC1
-#define IRQ_PIN PD2
+#define NUM_LEN 6
+#define ADC_PIN PC0
+#define OLED_ADDR 0x7B
 
 // Include necessary libraries
 #include <stdio.h>
@@ -26,41 +22,26 @@
 #include <stdint.h>
 #include <string.h>
 #include <avr/interrupt.h>
+#include <avr/eeprom.h>
 #include <util/delay.h>
-#include "i2cmaster.h"
-#include "lm75.h"
-#include "twi.h"
-#include "ssd1306.h"
+#include "ssd1306.h"      // SSD1306 v3.0.1 by Matiasus
 
 // I2C Communication Pins:   SCL -> PC5 | SDA -> PC4
 
-// Function prototypes
-int16_t main ( void ) ;
-void initADC ( void ) ;
-uint16_t readADC ( const uint8_t channel ) ;
-uint32_t map ( uint32_t x , uint32_t in_min , uint32_t in_max , uint32_t out_min , uint32_t out_max ) ;
-void addChars ( char *first , char *second , char *return_str ) ;
-void printLine ( uint32_t value , char *text , uint8_t x_pos , uint8_t y_pos ) ;
-ISR ( INT0_vect ) ;   // Interrupt Service Routine for PD2
-
-// Variables
-volatile uint8_t count = 0 , toggle = 1 ;
-char TEXT_BUFFER [ ROW_LEN ] ;
-char NUM_BUFFER [ NUM_LEN ] ;
-char MESSAGE [ ROW_LEN ] = "                    " ;
+typedef enum { INTEGER , DOUBLE } numtype_t ;
+typedef union { double dbl ; uint32_t intgr ; } number_t ;
 
 void initADC ( void ) {
-  ADMUX = 0x00 ;                            // Clear at start
-  ADMUX |= ( 1 << REFS0 ) ;                 // Set reference voltage to AVcc
+  ADMUX = ( 1 << REFS0 ) ;                 // Set reference voltage to AVcc
   ADCSRA |= ( 1 << ADEN ) | ( 1 << ADPS2 ) | ( 1 << ADPS1 ) | ( 1 << ADPS0 ) ;
   // Enable ADC and set prescaler to 128
 }
 
 uint16_t readADC ( const uint8_t channel ) {
-	ADMUX |= ( channel & 0x07 ) ;               // cut off channel value to the limited bits available
+	ADMUX |= ( channel & 0x07 ) ;               // Cut off channel value to the limited bits available
 	ADCSRA |= ( 1 << ADSC ) ;                   // Start ADC conversion
   while ( ADCSRA & ( 1 << ADSC ) ) ;          // Wait while the conversion completes
-  return ( ( uint16_t ) ADCL ) + ( ( uint16_t ) ADCH << 8 ) ;  // Bit-shift High nibble to get the result of the conversion
+  return ( ( uint16_t ) ADCL + ( ( uint16_t ) ADCH << 8 ) ) ;  // Bit-shift High nibble to get the result of the conversion
 }
 
 uint32_t map ( uint32_t x , uint32_t in_min , uint32_t in_max , uint32_t out_min , uint32_t out_max ) {
@@ -68,55 +49,58 @@ uint32_t map ( uint32_t x , uint32_t in_min , uint32_t in_max , uint32_t out_min
 }
 
 void addChars ( char *first , char *second , char *return_str ) {
-  uint8_t idx = 0 ;
-  char blanks [ ] = "     " ;
+  uint8_t ctr , idx = strlen ( first ) , offset = strlen ( second ) , remainder = strlen ( return_str ) - idx - offset ;
   
-  // You have to provide a semicolon character in the message, as it is used as the divider
-  while ( first [ idx ] != ':' ) { idx++ ; }
-
   // Copy the contents of the two char arrays into the buffer
-  memcpy ( return_str , first , strlen ( first ) ) ;
-  memcpy ( &return_str [ idx + 1 ] , &blanks [ 0 ] , 1 ) ;      // Add a blank character between displayed text and value
-	memcpy ( &return_str [ idx + 2 ] , second , strlen ( second ) ) ;   // Add the numerical value to the char array
-  memcpy ( &return_str [ idx + strlen ( second ) + 2 ] , blanks , strlen ( blanks ) ) ;   // Add some blank space characters at the end
+  memcpy ( return_str , first , idx ) ;
+	memcpy ( &return_str [ idx ] , second , offset ) ;       // Add the numerical value to the char array
+
+  if ( remainder > 0 ) {
+    char blanks [ remainder ] ;
+    for ( ctr = 0 ; ctr < remainder ; ctr++ ) {
+      blanks [ ctr ] = ' ' ;
+    }
+    memcpy ( &return_str [ idx + offset ] , blanks , remainder ) ;   // Add some blank space characters at the end
+  }
 }
 
-void printLine ( uint32_t value , char *text , uint8_t x_pos , uint8_t y_pos ) {
+void printLineWithNumber ( char *text , uint8_t x_pos , uint8_t y_pos , numtype_t NUMTYPE , number_t *NUMBER ) {
   // Use when displaying characters as well as numerical values
-  itoa ( value , NUM_BUFFER , 10 ) ;
-  memcpy ( MESSAGE , text , ROW_LEN ) ;
-  addChars ( MESSAGE , NUM_BUFFER , TEXT_BUFFER ) ;
+
+  char TEXT_BUFFER [ ROW_LEN ] , NUM_BUFFER [ NUM_LEN ] ;
+
+  switch ( NUMTYPE ) {
+   case INTEGER : itoa ( (int) NUMBER->intgr , NUM_BUFFER , 10 ) ; break ;
+   case DOUBLE : dtostrf ( NUMBER->dbl , 5 , 2 , NUM_BUFFER ) ; break ;
+   default : break ;
+  }
+  
+  addChars ( text , NUM_BUFFER , TEXT_BUFFER ) ;
   SSD1306_SetPosition ( x_pos , y_pos ) ;
   SSD1306_DrawString ( TEXT_BUFFER , NORMAL ) ;
 }
 
-ISR ( INT0_vect ) {
-  count++ ;
-  toggle = 1 ;
-  if ( count == 0xFF ) count = 0x00 ;
-}
+int main ( void ) {
 
-int16_t main ( void ) {
+  DDRC = ~( 1 << ADC_PIN ) ;    // Define the ADC Pin as Input
 
-  DDRD &= ~( 1 << IRQ_PIN ) ;   // Set up IRQ_PIN as Input
-  PORTD |= ( 1 << IRQ_PIN ) ;   // Enable Pull-up Resistor at IRQ_PIN
-  EICRA |= ( 1 << ISC01 ) ;     // Trigger interrupt on falling-edge
-  EIMSK |= ( 1 << INT0 ) ;      // Enable external interrupt INT0
+  number_t pctg , value ;                // ADC Reading
 
   // Initializing the display and ADC
   initADC ( ) ;
   SSD1306_Init ( ) ;
   SSD1306_ClearScreen ( ) ;
-
-  sei ( ) ; // Enable global interrupts
+  SSD1306_NormalScreen ( ) ;
+  SSD1306_SetPosition ( 0 , 0 ) ;
+  SSD1306_DrawString ( " SPROJ2EEG3" , BOLD ) ;
 
   // Infinite loop
   while ( 1 ) {
-    if ( toggle ) {
-      printLine ( count , "Counter:" , 0 , 0 ) ;
-      toggle = 0 ;
-    }
+    value.intgr = (uint32_t) readADC ( ADC_PIN ) ;
+    pctg.dbl = ( (double) value.intgr * 100.0 ) / 1023.0 ;   // Percentage
+
+    printLineWithNumber ("Reading: " , 0 , 50 , INTEGER , &value ) ;
+    printLineWithNumber ("Percentage: " , 0 , 70 , DOUBLE , &pctg ) ;
+    _delay_ms ( 100.0 ) ;
   }
-  
-  return 0 ;
 }
