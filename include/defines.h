971f32925be636922a65836a6c9f3c3407070342
Bence Tóth <bencike04201@gmail.com>
2024-06-20 13:57:35 +0200

    Added Battery voltage reading
    Added Battery voltage reading

--- include/defines.h
+++ include/defines.h
@@ -5,50 +5,51 @@
 #include <stdint.h>
 #include <math.h>
 
-#define Temp_Input PC1
-#define Bat_Input  PC2
+#define Temp_Input           PC1
+#define Bat_Input            PC2
 
-#define Left_Button  PD2
-#define Right_Button PD3
-#define Heart_Input  PD4
+#define Left_Button          PD2
+#define Right_Button         PD3
+#define Heart_Input          PD4
 
-#define ADC_MAX_VAL 1023.0F
-#define ADC_MIN_VAL 0.0F
+#define ADC_MAX_VAL          1023.0F
+#define ADC_MIN_VAL          0.0F
 
-#define OLED_WIDTH  128
-#define OLED_HEIGHT 64
+#define OLED_WIDTH           128
+#define OLED_HEIGHT          64
 
-#define START_POS_X  5
-#define MENU_GRAPH_Y 32
-#define MENU_DATA_Y  16
+#define START_POS_X          5
+#define MENU_GRAPH_Y         32
+#define MENU_DATA_Y          16
 
-#define CHAR_WIDTH        6
-#define CHAR_LENGTH       8
-#define MAX_CHARS_PER_NUM 5
-#define MAX_CHARS_IN_ROW  20
+#define CHAR_WIDTH           6
+#define CHAR_LENGTH          8
+#define MAX_CHARS_PER_NUM    5
+#define MAX_CHARS_IN_ROW     20
 
-#define VREF 5.0F
-#define BAT_MAX_VOLTAGE 4.2F
-#define BAT_MIN_VOLTAGE 3.0F
-#define BAT_MAX_READING floor ( ( ADC_MAX_VAL * BAT_MAX_VOLTAGE ) / VREF )
+#define VREF                 5.0F
+#define BAT_MAX_VOLTAGE      4.2F
+#define BAT_MIN_VOLTAGE      3.0F
+#define BAT_THRESH_VOLTAGE   3.1F
+#define BAT_MAX_READING      floor ( ( ADC_MAX_VAL * BAT_MAX_VOLTAGE ) / VREF )
 
 #define BPM_LOWER_LIMIT      30.0F
 #define BPM_UPPER_LIMIT      200.0F
 #define PULSE_BPM_MULTIPLIER 60.0F
 
-#define GRAPH_MAX_VERT_POS 9.0F
-#define GRAPH_MIN_VERT_POS 61.0F
+#define GRAPH_MAX_VERT_POS   9.0F
+#define GRAPH_MIN_VERT_POS   61.0F
 
-#define NUM_DATAPOINTS 20
-#define MIN_DATAPOINTS 2
+#define NUM_DATAPOINTS       20
+#define MIN_DATAPOINTS       2
 
-#define ARMPIT_MIN_TEMP 18.0F   // Minimum temperature reading - based on Op-amp circuit
-#define ARMPIT_MAX_TEMP 45.0F   // Maximum temperature reading - based on Op-amp circuit
+#define ARMPIT_MIN_TEMP      18.0F   // Minimum temperature reading - based on Op-amp circuit
+#define ARMPIT_MAX_TEMP      45.0F   // Maximum temperature reading - based on Op-amp circuit
 
-#define DECIMAL_PRECISION 2
-#define NUM_BASE          10
-#define MAX_BPM_SAMPLES   5
-#define MAX_TEMP_SAMPLES  250
+#define DECIMAL_PRECISION    2
+#define NUM_BASE             10
+#define MAX_BPM_SAMPLES      5
+#define MAX_TEMP_SAMPLES     250
 
 #define PRESCALER                1024.0F
 #define PULSE_GATHER_INTERVAL    floor ( 1000.0F / ( BPM_LOWER_LIMIT / PULSE_BPM_MULTIPLIER ) )  // Timer1 delay in milliseconds
@@ -70,6 +71,7 @@
 #define SWITCH_GRAPH           ( ( uint16_t ) ( 1 << 8  ) )
 #define BPM_EN                 ( ( uint16_t ) ( 1 << 9  ) )
 #define MENU_SELECTOR          ( ( uint16_t ) ( 1 << 10 ) )
+#define BAT_EN                 ( ( uint16_t ) ( 1 << 11 ) )
 
 #ifdef __cplusplus
 }
